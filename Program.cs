4a357b83b5848ba37b5e40edc4f9f6c21ce8d296
NicolasMilanese <nicolasmilanese2004@gmail.com>
2022-10-13 21:18:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ejercicio
+{
+class Program
+{
+static void Main (string[] args)
+{
+Console.Clear ();
+
+int entero = 1;
+bool boleano =true;
+string mensaje ="hola";
+decimal numero = 100;
+
+Console.WriteLine ( entero);
+Console.WriteLine ( boleano);
+Console.WriteLine ( mensaje);
+Console.WriteLine ( numero);
+}
+
+}
+
+}
